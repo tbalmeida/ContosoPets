6fd1d88e22f5cdd3f16df6d871330941a3210e93
Thiago Almeida <tbalmeida@gmail.com>
2020-10-21 17:26:02 -0600

    Added the first products

---
+++ ContosoPets/Data/ContosoPetsContext.cs
@@ -0,0 +1,25 @@
+﻿using ContosoPets.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContosoPets.Data
+{
+    class ContosoPetsContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<ProductOrder> ProductOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=ContosoPet;Persist Security Info=True;User ID=sa;Password=my@dminPass1209");
+        }
+    }
+}
--- ContosoPets/Program.cs
+++ ContosoPets/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ContosoPets.Data;
+using ContosoPets.Models;
+using System;
 
 namespace ContosoPets
 {
@@ -6,7 +8,24 @@ namespace ContosoPets
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using ContosoPetsContext context = new ContosoPetsContext();
+
+            Product squeakyBone = new Product()
+            {
+                Name = "Squeacky Dog Bone",
+                Price = 4.99M
+            };
+            context.Products.Add(squeakyBone);
+
+            Product tennisBalls = new Product()
+            {
+                Name = "Tennis Ball 3-Pack",
+                Price = 9.99M
+            };
+            context.Add(tennisBalls);
+
+            context.SaveChanges();
+
         }
     }
 }
